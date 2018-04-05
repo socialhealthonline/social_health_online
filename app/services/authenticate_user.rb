c2b1ec4392e9d551be996548c976d0f0c73739a3
Lee Smith <jeremyleesmith@gmail.com>
2018-04-05 10:15:19 -0500

    Handle admin with no member sign in

--- app/services/authenticate_user.rb
+++ app/services/authenticate_user.rb
@@ -10,14 +10,14 @@ class AuthenticateUser
   end
 
   def call
-    if @user && @user.authenticate(@params[:password]) && @user.enabled? && !@user.member.suspended
+    if @user && @user.authenticate(@params[:password]) && @user.enabled? && !@user.member&.suspended
       @authenticated = true
       @flash = "Welcome back, #{@user.name}!"
     else
       @authenticated = false
       if @user.nil? # no user
         @flash = 'The email or password you entered was not recognized. Please try again!'
-      elsif !@user.enabled? || @user.member.suspended
+      elsif !@user.enabled? || @user.member&.suspended
         @flash = 'Your account has been disabled!'
       elsif @user.enabled # bad password
         @flash = 'The email or password you entered was not recognized. Please try again!'
