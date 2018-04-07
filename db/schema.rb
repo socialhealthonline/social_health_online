d98fa6270dd307969b37b6fe0c0bfa18af8b7684
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-04-07 19:53:45 +0100

    Add Hide Info On Locator Field To Affiliate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_22_011716) do
+ActiveRecord::Schema.define(version: 2018_04_07_184917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_03_22_011716) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "hide_info_on_locator", default: false
   end
 
   create_table "events", force: :cascade do |t|
