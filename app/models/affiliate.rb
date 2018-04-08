4f0f1316522bba78849ab920fd0702280041cbc5
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-04-08 11:18:42 +0100

    Add validations for support_type and enumerize support_type in Affiliate model

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,8 +1,12 @@
 class Affiliate < ApplicationRecord
+  SUPPORT_TYPES = ["Events", "Discounts", "Events & Discounts" ].freeze
 
-  validates :name, :address, :city, :state, :zip, presence: true
+  enum support_types: SUPPORT_TYPES
+
+  validates :name, :address, :city, :state, :zip, :support_type, presence: true
   validates_uniqueness_of :name, case_sensitive: false
   validates :state, inclusion: US_STATES.values
+  validates :support_type, inclusion: Affiliate.support_types.values
   validates :zip, format: { with: %r{\A[\d]{5}(-[\d]{4})?\z} }
   validates :phone, format: { with: /\A\d{10}\z/, message: 'must be 10 digits including area code', allow_blank: true }
 
