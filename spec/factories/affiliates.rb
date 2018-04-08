cb3b5ad1ca7d83b54657bfe40e67c8bcb0e08bb5
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-04-08 11:20:17 +0100

    Add support_type to Affiliate factory

--- spec/factories/affiliates.rb
+++ spec/factories/affiliates.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :affiliate do
-    name 'Affiliate LLC'
-    address '123 Main St.'
-    city 'Hometown'
-    state 'AL'
-    zip '35210'
+    name "Affiliate LLC"
+    address "123 Main St."
+    city "Hometown"
+    state "AL"
+    zip "35210"
+    support_type { Affiliate::SUPPORT_TYPES.sample }
   end
 end
