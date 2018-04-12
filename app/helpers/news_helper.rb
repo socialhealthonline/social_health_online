b850f55de2133205cf2598ce528ddb014de664b5
Andrey Tykva <tykva.andrey@gmail.com>
2018-04-12 20:02:30 +0300

    Add date helpers for news

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -6,4 +6,12 @@ module NewsHelper
     icon = column == sort_column ? icon : ''
     link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
   end
+
+  def date_for_news(date)
+    date.blank? ? nil : date.strftime('%m/%d/%Y')
+  end
+
+  def date_time_for_news(date)
+    date.blank? ? nil : date.strftime('%Y-%m-%d %H:%M')
+  end
 end
