089d6804468434f73d6ba20570cf6475393cd6ed
Lee Smith <jeremyleesmith@gmail.com>
2018-04-12 11:54:06 -0500

    Revert puma config change for dev mode

--- config/puma.rb
+++ config/puma.rb
@@ -21,11 +21,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-if Rails.env.development?
-  workers 1
-else
-  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-end
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
