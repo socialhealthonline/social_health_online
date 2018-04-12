76fe23019b1e959e65e80e39cc0abaef51c8fd0c
Lee Smith <jeremyleesmith@gmail.com>
2018-04-11 22:27:08 -0500

    Add community page

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,7 +2,7 @@ class CommunitiesController < ApplicationController
   before_action :require_authentication
 
   def show
-    @community = Member.friendly.find params[:id]
+    @member = Member.friendly.find params[:id]
   end
 
 end
