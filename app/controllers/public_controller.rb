b6ff63a46459c35d1739905253b5c57a07de866e
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-04-17 02:47:30 +0100

    affiliates json endpoint

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -24,4 +24,30 @@ class PublicController < ApplicationController
   def affiliate_locator
     @affiliates = Affiliate.where(hide_info_on_locator: false)
   end
+
+  def affiliates
+    state = params[:state]
+    city = params[:city]
+    zip = params[:zip]
+
+    state = state.downcase.strip if state
+    city = city.downcase.strip if city
+    zip = zip.strip if zip
+
+    affiliates = Affiliate.where(hide_info_on_locator: false)
+
+    if city && state
+      where('name LIKE ?', "%#{search}%")
+
+      affiliates = affiliates.where(["lower(state) LIKE :state AND lower(city) LIKE :city", {state: "%#{state}%", city: "%#{city}%"}])
+    elsif city
+      affiliates = affiliates.where(["lower(city) LIKE :city", {city: "%#{city}%"}])
+    elsif state
+      affiliates = affiliates.where(["lower(state) LIKE :state", {state: "%#{state}%"}])
+    elsif zip
+      affiliates = affiliates.where(zip: zip)
+    end
+
+    render json: affiliates, each_serializer: AffiliatesForLocatorSerializer
+  end
 end
