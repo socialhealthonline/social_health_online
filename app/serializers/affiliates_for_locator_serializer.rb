cc06cae7e59bd3ae7d3e2725fcd3275a26d1c506
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-04-17 02:46:33 +0100

    Add Affiliates Serializer

---
+++ app/serializers/affiliates_for_locator_serializer.rb
@@ -0,0 +1,11 @@
+class AffiliatesForLocatorSerializer < ActiveModel::Serializer
+  attributes :name, :address, :city, :zip, :url, :phone, :support_type, :full_address, :state
+
+  def support_type
+    Affiliate::SUPPORT_TYPES[object.support_type]
+  end
+
+  def state
+    US_STATES.key(object.state)
+  end
+end
