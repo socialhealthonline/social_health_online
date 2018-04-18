d76ba702b10c6eda1effabe607453385f70b77e9
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-04-18 13:13:50 +0100

    implement  /members endpoint

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -22,4 +22,61 @@ class PublicController < ApplicationController
     @news = News.order('updated_at desc').page(params[:page])
   end
 
+  def affiliate_locator
+  end
+
+  def member_locator
+  end
+
+  def affiliates
+    state = params[:state]
+    city = params[:city]
+    zip = params[:zip]
+
+    state = state.downcase.strip if state
+    city = city.downcase.strip if city
+    zip = zip.strip if zip
+
+    affiliates = Affiliate.where(hide_info_on_locator: false)
+
+    if city && state
+      where('name LIKE ?', "%#{search}%")
+
+      affiliates = affiliates.where(["lower(state) LIKE :state AND lower(city) LIKE :city", {state: "%#{state}%", city: "%#{city}%"}])
+    elsif city
+      affiliates = affiliates.where(["lower(city) LIKE :city", {city: "%#{city}%"}])
+    elsif state
+      affiliates = affiliates.where(["lower(state) LIKE :state", {state: "%#{state}%"}])
+    elsif zip
+      affiliates = affiliates.where(zip: zip)
+    end
+
+    render json: affiliates, each_serializer: AffiliateForLocatorSerializer
+  end
+
+  def members
+    state = params[:state]
+    city = params[:city]
+    zip = params[:zip]
+
+    state = state.downcase.strip if state
+    city = city.downcase.strip if city
+    zip = zip.strip if zip
+
+    members = Member.where(hide_info_on_locator: false)
+
+    if city && state
+      where('name LIKE ?', "%#{search}%")
+
+      members = members.where(["lower(state) LIKE :state AND lower(city) LIKE :city", {state: "%#{state}%", city: "%#{city}%"}])
+    elsif city
+      members = members.where(["lower(city) LIKE :city", {city: "%#{city}%"}])
+    elsif state
+      members = members.where(["lower(state) LIKE :state", {state: "%#{state}%"}])
+    elsif zip
+      members = members.where(zip: zip)
+    end
+
+    render json: members, each_serializer: MemberForLocatorSerializer
+  end
 end
