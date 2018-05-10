819628cb6912421b2c3370d262779b284b0501af
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-05-10 07:20:04 +0100

    types, categories and ratings constants

---
+++ config/initializers/event_types.rb
@@ -0,0 +1,51 @@
+EVENT_TYPES = [
+  "Art/Photography",
+  "Career/Business",
+  "Dance/Theater", 
+  "Family/Kids", 
+  "Fashion/Beauty", 
+  "Festivals/Fairs",
+  "Film",
+  "Food/Drink",
+  "Games",
+  "Health/Wellness",
+  "Hobbies/Crafts",
+  "Language/Culture",
+  "Learning/Education",
+  "Music",
+  "Nightlife",
+  "Other",
+  "Outdoor/Adventure",
+  "Pets/Animals",
+  "Reading/Writing",
+  "Religion/Beliefs",
+  "Seniors",
+  "Social Movements",
+  "Sports/Fitness",
+  "Tech",
+  "Volunteer/Charity"
+].freeze
+
+EVENT_CATEGORIES = [
+  "Affiliate",
+  "Community",
+  "MatchMaker",
+  "Weekly Social Target"
+].freeze
+
+RATINGS = {
+  "Very Good": 4,
+  "Good": 3,
+  "OK": 2,
+  "Poor": 1,
+  "Very Poor": 0
+}
+
+RATINGS_WITH_NA = {
+  "Very Good": 5,
+  "Good": 4,
+  "OK": 3,
+  "Poor": 2,
+  "Very Poor": 1,
+  "N/A": 0
+}
