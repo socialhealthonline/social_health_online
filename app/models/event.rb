1fb66d5b178e28f948c209782b5318e33a39d6e0
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-05-10 07:21:27 +0100

    use EVENT_TYPES

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,8 @@
 class Event < ApplicationRecord
-  TYPES = ['Art/Photography', 'Career/Business', 'Dance/Theater', 'Family/Kids', 'Fashion/Beauty', 'Festivals/Fairs', 'Film', 'Food/Drink', 'Games', 'Health/Wellness', 'Hobbies/Crafts', 'Language/Culture', 'Learning/Education', 'Music', 'Nightlife', 'Other', 'Outdoor/Adventure', 'Pets/Animals', 'Reading/Writing', 'Religion/Beliefs', 'Seniors', 'Social Movements', 'Sports/Fitness', 'Tech', 'Volunteer/Charity' ]
-
   belongs_to :member, inverse_of: :events
 
   validates :title, :start_at, :event_type, presence: true
-  validates :event_type, inclusion: TYPES
+  validates :event_type, inclusion: EVENT_TYPES
   validates :state, inclusion: US_STATES.values, allow_nil: true
 
   before_validation :add_protocol_to_url
