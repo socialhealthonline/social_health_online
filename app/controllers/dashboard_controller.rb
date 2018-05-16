ca17f8e155b992648ba2cee0d7d356717186d373
Dmitry <d.batracov@sloboda-studio.com>
2018-05-16 17:59:17 +0300

    Events feed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
 
   def index
     @notifications = Notification.page(params[:page])
+    @events = Event.joins(:rsvps).where("rsvps.user_id = ? and rsvps.rsvp_status in (?) and start_at >= ?",
+                                        authenticated_user.id, ['Maybe', 'Yes'], Time.use_zone('UTC') { Time.zone.now })
+                                        .select('events.*, rsvps.rsvp_status as status')
   end
-
 end
