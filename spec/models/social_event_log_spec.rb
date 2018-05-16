613853de22803bbfa54fe713e5256b56c5fd53bb
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-05-16 11:10:02 +0100

    Fix Social Event Log tests

--- spec/models/social_event_log_spec.rb
+++ spec/models/social_event_log_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe SocialEventLog, type: :model do
     it { should validate_presence_of :event_date }
     it { should validate_presence_of :state }
     it { should validate_presence_of :city }
-    it { should validate_presence_of :type }
+    it { should validate_presence_of :event_type }
     it { should validate_presence_of :source }
     it { should validate_presence_of :rating }
     it { should validate_inclusion_of(:state).in_array(US_STATES.values) }
-    it { should validate_inclusion_of(:type).in_array(EVENT_TYPES) }
+    it { should validate_inclusion_of(:event_type).in_array(EVENT_TYPES) }
     it { should validate_inclusion_of(:source).in_array(SocialEventLog::EVENT_SOURCES.values) }
     it { should validate_inclusion_of(:rating).in_array(RATINGS.values) }
   end
