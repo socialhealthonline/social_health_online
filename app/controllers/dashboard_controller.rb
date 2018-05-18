d5b35518a6c9af245bbc0db9aebde0e3f29eb227
Dmitry <d.batracov@sloboda-studio.com>
2018-05-18 15:08:14 +0300

    Move query in scope and add new line to the end of the files

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,6 @@ class DashboardController < ApplicationController
 
   def index
     @notifications = Notification.page(params[:page])
-    @events = Event.joins(:rsvps).where("rsvps.user_id = ? and rsvps.rsvp_status in (?) and start_at >= ?",
-                                        authenticated_user.id, ['Maybe', 'Yes'], Time.use_zone('UTC') { Time.zone.now })
-                                        .select('events.*, rsvps.rsvp_status as status')
+    @events = Event.joins(:rsvps).events_for_feed(authenticated_user.id)
   end
 end
