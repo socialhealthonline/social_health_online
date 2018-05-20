2fca9c9448a88f4fcaf68762698cdf5396bdbccf
Ifedayo Oluwadamilola <sureife@gmail.com>
2018-05-20 18:11:48 +0100

    Multi-Select dropdowna for event_type and event_category

--- app/models/event_category.rb
+++ app/models/event_category.rb
@@ -1,6 +1,6 @@
 class EventCategory < ApplicationRecord
   belongs_to :social_event_log
   validates_presence_of :name
-  validates_uniqueness_of :name, scope: :social_event_log_id
-  # validates :name, uniqueness: { scope: :social_event_log, message: "no duplicate categories" }
+
+  validates :name, inclusion: EVENT_CATEGORIES
 end
