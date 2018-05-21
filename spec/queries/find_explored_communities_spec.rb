d4b3654c292f459d37762e055a0c0b36a14fdc93
Dmitry <d.batracov@sloboda-studio.com>
2018-05-21 09:45:55 +0300

    Test for queries object

--- spec/queries/find_explored_communities_spec.rb
+++ spec/queries/find_explored_communities_spec.rb
@@ -2,22 +2,67 @@ require 'rails_helper'
 
 RSpec.describe FindExploredCommunities do
   describe '#call' do
-    context 'given state filter' do
-      it 'filters by state' do
+    let(:member_1) { create(:member) }
+    let(:user) { create(:user, member: member_1) }
 
+    before do
+      @params = {}
+      @params[:state] = 'AL'
+      @params[:city] = 'Hometown'
+      @params[:zip] = '35210'
+    end
+
+    context 'success filter' do
+      let!(:member_2) { create(:member) }
+
+      before do
+        @communities = Member.where("name !=? ", user.member.name)
+        @communities = FindExploredCommunities.new(@communities).call(@params)
+      end
+
+      context 'given state filter' do
+        it 'filters by state' do
+          expect(@communities.size).to eq(1)
+        end
+      end
+
+      context 'given city filter' do
+        it 'filters by city' do
+          expect(@communities.size).to eq(1)
+        end
+      end
+
+
+      context 'given zip filter' do
+        it 'filters by zip' do
+          expect(@communities.size).to eq(1)
+        end
       end
     end
 
-    context 'given city filter' do
-      it 'filters by city' do
-
+    context 'unsuccess filter' do
+      before do
+        @communities = Member.where("name !=? ", user.member.name)
+        @communities = FindExploredCommunities.new(@communities).call(@params)
       end
-    end
+
+      context 'empty state filter' do
+        it 'filters by state' do
+          expect(@communities.size).to eq(0)
+        end
+      end
+
+      context 'empty city filter' do
+        it 'filters by city' do
+          expect(@communities.size).to eq(0)
+        end
+      end
 
 
-    context 'given zip filter' do
-      it 'filters by zip' do
-
+      context 'empty zip filter' do
+        it 'filters by zip' do
+          expect(@communities.size).to eq(0)
+        end
       end
     end
   end
