aa2b4c36aebc2c186e37daabfa3b247ae30c7f88
Dmitry <d.batracov@sloboda-studio.com>
2018-05-22 14:19:22 +0300

    Fix tests

--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Rsvp, type: :model do
+  let(:member) { create(:member) }
+  let(:user) { create(:user, member: member) }
+  let(:event) { create(:event, member: member)}
+  subject { create(:rsvp, event: event, user: user) }
 
   it { should have_db_index(:event_id) }
   it { should have_db_index(:user_id) }
