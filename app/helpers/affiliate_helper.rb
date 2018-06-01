57f8de620337e460274d3bd5a7f03f4f0f86e187
Acellam Guy <abiccel@yahoo.com>
2018-06-01 21:10:38 +0300

    Change getting of records based as per quality checklist

--- app/helpers/affiliate_helper.rb
+++ app/helpers/affiliate_helper.rb
@@ -10,12 +10,12 @@ module AffiliateHelper
   def csv_affilialate_list
     columns = %w[name address city state zip phone url]
     CSV.generate(col_sep: ';',
-                 row_sep: "\r\n",
+                 row_sep: "\n",
                  headers: true,
                  write_headers: true,
                  return_headers: true) do |csv|
       csv << %w[Name Address City State Zip Phone URL]
-      Affiliate.all.each do |affiliate|
+      Affiliate.find_each do |affiliate|
         csv << columns.collect { |name| affiliate.send(name) }
       end
     end
