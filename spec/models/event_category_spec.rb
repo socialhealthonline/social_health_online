479933fd656299b39b52046891acfec03dfc07d5
Andrey Tykva <tykva.andrey@gmail.com>
2018-06-03 23:47:38 +0300

    Add modal for community profile

--- spec/models/event_category_spec.rb
+++ spec/models/event_category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EventCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:social_event_log) }
+  it { should validate_presence_of :name }
+  it { should validate_inclusion_of(:name).in_array(EVENT_CATEGORIES) }
 end
