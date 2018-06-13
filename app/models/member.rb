94192d402803df1e1bcfc936688a35dcb402a021
Andrey Tykva <tykva.andrey@gmail.com>
2018-06-13 20:54:31 +0300

    Fix falling tests

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,14 +8,14 @@ class Member < ApplicationRecord
   has_many :announcements, dependent: :destroy
   has_one_attached :logo
 
-  validates :name, :address, :city, :state, :zip, :contact_name, :contact_email, :contact_phone, :service_capacity, presence: true
+  validates :name, :address, :city, :state, :zip, :contact_name, :contact_email, :contact_phone, :phone, :service_capacity, presence: true
   validates_uniqueness_of :name, case_sensitive: false
   validates :state, inclusion: US_STATES.values
-  validates :zip, format: {with: %r{\A[\d]{5}(-[\d]{4})?\z}}
-  validates :contact_phone, format: {with: /\A\d{10}\z/, message: "must be 10 digits including area code"}
+  validates :zip, format: { with: %r{\A[\d]{5}(-[\d]{4})?\z} }
+  validates :contact_phone, format: { with: /\A\d{10}\z/, message: "must be 10 digits including area code" }
   validates_format_of :contact_email, with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
   validate :logo_validation
-  validates :phone, format: {with: /\A\d{10}\z/, message: "must be 10 digits including area code"}
+  validates :phone, format: { with: /\A\d{10}\z/, message: "must be 10 digits including area code" }
 
   before_validation { |member| member.contact_phone.gsub!(/\D/, "") if member.contact_phone? }
   before_validation :add_protocol_to_url
