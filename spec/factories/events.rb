e284400e25a605b854d797903a3aaab66129e462
Andrey Tykva <tykva.andrey@gmail.com>
2018-06-15 16:28:44 +0300

    Fix tests for events

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :event do
     title 'Event Title'
+    address '123 Main St.'
+    city 'Hometown'
+    zip '35210'
+    location 'Somewhere'
     start_at Time.current
     event_type EVENT_TYPES.sample
   end
