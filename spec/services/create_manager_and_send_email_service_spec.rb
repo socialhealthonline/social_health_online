b09851ef0c1d852e21770af027de0fc22f9e3ab1
Dmitry <d.batracov@sloboda-studio.com>
2018-06-26 14:16:32 +0300

    Refactoring test, add validation for users capacity and add some css for member registration forms

--- spec/services/create_manager_and_send_email_service_spec.rb
+++ spec/services/create_manager_and_send_email_service_spec.rb
@@ -2,19 +2,12 @@ require 'rails_helper'
 
 describe CreateManagerAndSendEmailService do
   let!(:member) { create(:member) }
-
-  before do
-    @service = CreateManagerAndSendEmailService.new(member, 'Test user name', 'example@gmail.com')
-  end
+  let!(:service) { CreateManagerAndSendEmailService.new(member, 'Test user name', 'example@gmail.com') }
 
   describe '#call' do
     context 'Register New User' do
-      before do
-        @service.call
-      end
-
       it 'send email' do
-        expect(ActionMailer::Base.deliveries.count).to eq(1)
+        expect{ service.call }.to change{ ActionMailer::Base.deliveries.count }.by(1)
       end
     end
   end
