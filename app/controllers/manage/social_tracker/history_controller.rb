c167e7a00f8681f646c6d87f7bcfc6df98e708d9
Alex Ulbricht <alex.a.ulbricht@gmail.com>
2018-06-28 17:22:35 -0400

    Restoring old fixes

--- app/controllers/manage/social_tracker/history_controller.rb
+++ app/controllers/manage/social_tracker/history_controller.rb
@@ -1,6 +1,4 @@
 class Manage::SocialTracker::HistoryController < ApplicationController
-  helper_method :sort_column, :sort_direction
-
   def users
     @member = Member.includes(users: [:social_event_logs]).friendly.find(params[:name])
   end
