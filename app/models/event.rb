ad1fca0e583c7bef05eca0e5e065a050bd1619a2
Alex Ulbricht <alex.a.ulbricht@gmail.com>
2018-07-04 20:02:42 -0400

    Fixed locators sorting icons and setup events search updates

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,6 +13,11 @@ class Event < ApplicationRecord
                                                        authenticated_user_id, [Rsvp.rsvp_statuses[:yes], Rsvp.rsvp_statuses[:maybe]], Time.zone.now)
                                                        .select('events.*, rsvps.rsvp_status as status') }
 
+  scope :searchevents_for_feed, -> (authenticated_user_id) { where("rsvps.user_id = ? and rsvps.rsvp_status in (?) and start_at >= ?",
+                                                      authenticated_user_id, [Rsvp.rsvp_statuses[:yes]], Time.zone.now)
+                                                      .select('events.*, rsvps.rsvp_status as status') }
+
+
   def location_display
     [address, city, state, zip].join(', ')
   end
