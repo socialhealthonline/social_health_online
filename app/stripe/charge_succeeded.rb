301ae6a065353a4095092a4da972ab133d3b39d0
Andrey Tykva <tykva.andrey@gmail.com>
2018-07-16 19:43:03 +0300

    Disable markers for each item

--- app/stripe/charge_succeeded.rb
+++ app/stripe/charge_succeeded.rb
@@ -2,6 +2,6 @@ class ChargeSucceeded
   def call(event)
     invoice = event.data.object
     member = Member.find_by(stripe_customer_id: invoice.customer)
-    member.update(suspended: false) if member
+    member.update(suspended: false, account_start_date: Time.current) if member
   end
 end
