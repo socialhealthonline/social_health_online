4a61968a95c9b622659e67a8f761a34385fd0960
Dmitry <d.batracov@sloboda-studio.com>
2018-07-19 11:13:36 +0300

    Edit the braces in correct way

--- app/assets/javascripts/stripe_form.js
+++ app/assets/javascripts/stripe_form.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
     displayError.empty();
     }
   });
-
+ 
   $('#payment-form').submit(function( event ) {
     if(document.getElementById('creditOrDebit').checked) {
       event.preventDefault();
