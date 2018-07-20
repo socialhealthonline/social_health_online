41c84b3b94184aa3c1227903b6949778e3faf8a8
Dmitry <d.batracov@sloboda-studio.com>
2018-07-20 15:30:51 +0300

    Wrap update with if statement

--- app/stripe/subscription_created.rb
+++ app/stripe/subscription_created.rb
@@ -2,9 +2,11 @@ class SubscriptionCreated
   def call(event)
     invoice = event.data.object
     member = Member.find_by(stripe_customer_id: invoice.customer)
-    member.update( account_start_date: Time.at(invoice.current_period_start),
-                  account_end_date: Time.at(invoice.current_period_end),
-                  period: invoice.plan.nickname,
-                  suspended: false ) if member
+    if member
+      member.update( account_start_date: Time.at(invoice.current_period_start),
+                     account_end_date: Time.at(invoice.current_period_end),
+                     period: invoice.plan.nickname,
+                     suspended: false )
+    end
   end
 end
