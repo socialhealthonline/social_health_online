3a43c9bc8e9e2477a5903fc915056d9de120c6c7
Dmitry <basher9696@gmail.com>
2018-07-25 10:27:55 +0300

    Rewrite services tests

--- spec/services/authenticate_user_spec.rb
+++ spec/services/authenticate_user_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
 describe AuthenticateUser do
-  let(:authenticate_user) { instance_double(AuthenticateUser) }
-  let(:disabled_user) { double('user', user_status: :disabled) }
-
+  let(:user) { create(:user, member: create(:member), password: '012345678qwertY', password_confirmation: '012345678qwertY' )  }
+  let(:disabled_user) { create(:user, user_status: :disabled, password: '012345678qwertY', password_confirmation: '012345678qwertY', member: create(:member)) }
+  let(:params) { { password: '012345678qwertY' } }
+  
   describe '#call' do
-    before do
-      allow(authenticate_user).to receive(:success?).and_return(true)
-    end
+    context 'successfully authenticate user' do
+      let(:authenticate_user) { AuthenticateUser.new(user, params) }
+      before { authenticate_user.call }
 
-    it 'successfully authenticate user' do
-      expect(authenticate_user.success?).to eq(true)
+      it 'successfully authenticate user' do
+        expect(authenticate_user.success?).to eq(true)
+      end
+
+      it 'successfully authenticate user flash' do
+        expect(authenticate_user.flash).to eq("Welcome back, #{user.name}!")
+      end
     end
 
     context 'user was not found' do
-      before do
-        allow(authenticate_user).to receive(:flash).and_return('The email or password you entered was not recognized. Please try again!')
-      end
+      let(:authenticate_user) { AuthenticateUser.new(nil, params) }
+      before { authenticate_user.call }
 
       it 'flash with no user found' do
         expect(authenticate_user.flash).to eq('The email or password you entered was not recognized. Please try again!')
@@ -24,17 +29,15 @@ describe AuthenticateUser do
     end
 
     context 'was found disabled user' do
-      before do
-        allow(disabled_user).to receive(:disabled?).and_return(true)
-        allow(disabled_user).to receive(:flash).and_return('Your account has been disabled!')
-      end
+      let(:authenticate_user) { AuthenticateUser.new(disabled_user, params) }
+      before { authenticate_user.call }
 
       it 'disabled user' do
         expect(disabled_user.disabled?).to eq(true)
       end
 
       it 'flash with disabled user' do
-        expect(disabled_user.flash).to eq('Your account has been disabled!')
+        expect(authenticate_user.flash).to eq('Your account has been disabled!')
       end
     end
   end
