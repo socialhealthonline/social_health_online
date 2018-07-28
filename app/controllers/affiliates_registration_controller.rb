348c91dfcfe9c12eccab88e8d9ed6c5b1ccfa54a
Alex Ulbricht <alex.a.ulbricht@gmail.com>
2018-07-28 16:46:03 -0400

    Added captchas to reduce spam

--- app/controllers/affiliates_registration_controller.rb
+++ app/controllers/affiliates_registration_controller.rb
@@ -6,9 +6,12 @@ class AffiliatesRegistrationController < ApplicationController
   def create
     if params[:subject].present?
       redirect_to affiliates_registration_url
-    else
+    elsif verify_recaptcha
       AffiliatesRegistrationMailer.notify(params).deliver_now
       redirect_to affiliates_registration_url, success: "Thank you for your interest in becoming an Affiliate. We will review your submission and follow up with you soon!"
+    else
+      flash.now[:error] = 'Please correct the errors to continue.'
+      render :new
     end
   end
 end
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,10 +6,13 @@ class ContactController < ApplicationController
   def create
     if params[:subject].present? # ignore scripted submissions
       redirect_to contact_url
-    else
+    elsif verify_recaptcha
       # send email
       ContactMailer.notify(params).deliver_now
       redirect_to contact_url, success: "Thanks for contacting us. We'll be in touch!"
+    else
+      flash.now[:error] = 'Please correct the errors to continue.'
+      render :new
     end
   end
 
