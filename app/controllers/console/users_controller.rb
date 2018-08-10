90e09d7171e9f251306ed3da2bc89afa81df62f1
Alex Ulbricht <alex.a.ulbricht@gmail.com>
2018-08-10 09:38:42 -0400

    Misc enhancements

--- app/controllers/console/users_controller.rb
+++ app/controllers/console/users_controller.rb
@@ -4,7 +4,6 @@ class Console::UsersController < ConsoleController
   helper_method :sort_column, :sort_direction
 
   def index
-    @users = User.where(member_id: @member.id).order(:name).page(params[:page]).per(25)
     @users = User.where(member_id: @member.id).order("#{sort_column} #{sort_direction}").page(params[:page]).per(25)
   end
 
