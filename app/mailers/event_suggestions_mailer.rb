abdd35c558270d0434b9ad45d8cf8826e81f0fdc
Alex Ulbricht <alex.a.ulbricht@gmail.com>
2018-08-10 13:04:30 -0400

    Finished event suggestions and misc updates

--- app/mailers/event_suggestions_mailer.rb
+++ app/mailers/event_suggestions_mailer.rb
@@ -2,9 +2,8 @@ class EventSuggestionsMailer < ApplicationMailer
   def notify(form_params, authenticated_user)
     @form_params = form_params
     @authenticated_user = authenticated_user
-    email = @form_params[:email]
 
-    mail :to => email,
+    mail :to => '@member.primary_manager_id&.email',
          subject: 'Event Suggestions - Social Health Online',
          from: "#{authenticated_user.name} <#{authenticated_user.email}>"
   end
